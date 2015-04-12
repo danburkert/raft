3728fca2b0a6bdfeede6881746143c1f851cdad7
Dan Burkert <dan@danburkert.com>
2015-04-11 23:20:22 -0700

    fix for 1.0-beta

--- tests/hello.rs
+++ tests/hello.rs
@@ -8,10 +8,8 @@ fn hello() {
     let mut nodes = new_cluster(3);
     let sent_command = b"Hello";
     // Push
-    nodes[0].0.append(sent_command)
-        .ok().expect("Couldn't append.");
+    nodes[0].0.append(sent_command).unwrap();
     // Retrieve.
-    let recieved_command = nodes[0].1.recv()
-        .ok().expect("Couldn't recv.");
-    assert_eq!(sent_command, recieved_command);
+    let recieved_command = nodes[0].1.recv().unwrap();
+    assert_eq!(sent_command, &*recieved_command);
 }
